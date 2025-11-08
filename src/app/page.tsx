227b12d10ea169af1a2269e397a8675912a33cfd
himanshumannu51-collab <himanshu.mannu51@gmail.com>
2025-11-08 19:40:32 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
             </p>
 
             <div className="flex flex-col sm:flex-row gap-4">
-              <Link href="/builder">
+              <Link href="/onboarding">
                 <Button size="lg" className="text-lg px-8 py-6 w-full sm:w-auto bg-indigo-600 hover:bg-indigo-700">
                   <Zap className="w-5 h-5 mr-2" />
                   Create Resume - Free
